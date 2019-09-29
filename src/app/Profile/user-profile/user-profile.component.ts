538e1da2f73b6aaa6db5485495630c479e384dce
brian23-eng <b.odhiambo.bo@gmail.com>
2019-09-29 14:14:58 +0300

    add a form to input search values

--- src/app/Profile/user-profile/user-profile.component.ts
+++ src/app/Profile/user-profile/user-profile.component.ts
@@ -1,17 +1,23 @@
+import { User } from './../../Classes/user';
 import { HttpClient } from '@angular/common/http';
 import { ProfileService } from './../../Service/profile.service';
 import { Component, OnInit } from '@angular/core';
 
 
 
+
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
+  providers: [ProfileService],
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { 
+  user:User;
+  username :string;
+
+  constructor(private profileService:ProfileService, private httpClient:HttpClient) { 
   
   }
 
