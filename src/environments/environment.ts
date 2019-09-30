5d62bd2ebb486159282b666e0133419e1addcdc0
brian23-eng <b.odhiambo.bo@gmail.com>
2019-09-30 12:19:57 +0300

    debug

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl:"https://api.github.com/users/",
-  apiKey:'1744a1eb2a2c0fb3c3afba62d2f972826066c2c8',
-  apiUserUrl:"https://api.github.com/users/brian23-eng?access_token=d5250069c5dd8749aa8f5e2601c0c3b1702f40d9",
+  // apiUrl:"https://api.github.com/users/",
+  apiKey:'1744a1eb2a2c0fb3c3afba62d2f972826066c2c8'
 };
 
 /*
